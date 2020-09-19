3f3c3b1aeca632eea711a182b5f0f3ef3a9d9a0c
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-09-19 01:13:17 -0300

    checkbox en aside

--- includes/aside.php
+++ includes/aside.php
@@ -1,63 +1,69 @@
 <div class="col-lg-3">
 
-            
-<img class="pt-5" src="img/elbardoinmortal.png" alt="logo libreria">
-<h1 class="pt-5">El Bardo Inmortal</h1>
 
-<ul class="list-group pt-3">
-    <h6>Editoriales</h6>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Planeta</a>
-        <span class="badge badge-primary badge-pill">3</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Alfaguara</a>
-        <span class="badge badge-primary badge-pill">7</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">De Bolsillo</a>
-        <span class="badge badge-primary badge-pill">8</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Salamandra</a>
-        <span class="badge badge-primary badge-pill">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Siglo XXI</a>
-        <span class="badge badge-primary badge-pill">11</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Anagrama</a>
-        <span class="badge badge-primary badge-pill">4</span>
-    </li>
-</ul>
-<ul class="list-group pt-5">
-    <h6>Generos</h6>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Terror</a>
-        <span class="badge badge-primary badge-pill">3</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Drama</a>
-        <span class="badge badge-primary badge-pill">7</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Suspenso</a>
-        <span class="badge badge-primary badge-pill">8</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Policial</a>
-        <span class="badge badge-primary badge-pill">14</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Lectura ligera</a>
-        <span class="badge badge-primary badge-pill">11</span>
-    </li>
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#" class="font-weight-bold">Fatnasia</a>
-        <span class="badge badge-primary badge-pill">4</span>
-    </li>
-</ul>
+    <img class="pt-5" src="img/elbardoinmortal.png" alt="logo libreria">
+    <h1 class="pt-5">El Bardo Inmortal</h1>
+
+    <ul class="list-group pt-3">
+        <h6>Editoriales</h6>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input " id="customCheck1">
+                <label class="custom-control-label" for="customCheck1">Salamandra</label>
+                
+            </div>
+            <span class="badge badge-primary badge-pill">3</span>
+        </li>
+
+
+       
+
+
+
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">De Bolsillo</a>
+            <span class="badge badge-primary badge-pill">8</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Salamandra</a>
+            <span class="badge badge-primary badge-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Siglo XXI</a>
+            <span class="badge badge-primary badge-pill">11</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Anagrama</a>
+            <span class="badge badge-primary badge-pill">4</span>
+        </li>
+    </ul>
+    <ul class="list-group pt-5">
+        <h6>Generos</h6>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Terror</a>
+            <span class="badge badge-primary badge-pill">3</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Drama</a>
+            <span class="badge badge-primary badge-pill">7</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Suspenso</a>
+            <span class="badge badge-primary badge-pill">8</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Policial</a>
+            <span class="badge badge-primary badge-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Lectura ligera</a>
+            <span class="badge badge-primary badge-pill">11</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <a href="#" class="font-weight-bold">Fatnasia</a>
+            <span class="badge badge-primary badge-pill">4</span>
+        </li>
+    </ul>
 
 
 </div>
