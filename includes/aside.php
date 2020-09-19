c3216f07db4df659c71bb9d87e76d1f264a5430f
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-09-19 02:10:36 -0300

    checkbox terminados

--- includes/aside.php
+++ includes/aside.php
@@ -3,67 +3,105 @@
 
     <img class="pt-5" src="img/elbardoinmortal.png" alt="logo libreria">
     <h1 class="pt-5">El Bardo Inmortal</h1>
+    <form>
 
-    <ul class="list-group pt-3">
-        <h6>Editoriales</h6>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input " id="customCheck1">
-                <label class="custom-control-label" for="customCheck1">Salamandra</label>
-                
+
+        <ul class="list-group pt-3">
+            <h6>Editoriales</h6>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck1">
+                    <label class="custom-control-label" for="customCheck1">Planeta</label>
+                </div>
+                <span class="badge badge-primary badge-pill">3</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck2">
+                    <label class="custom-control-label" for="customCheck2">De Bolsillo</label>
+                </div>
+                <span class="badge badge-primary badge-pill">8</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck3">
+                    <label class="custom-control-label" for="customCheck3">Salamandra</label>
+                </div>
+                <span class="badge badge-primary badge-pill">7</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck4">
+                    <label class="custom-control-label" for="customCheck4">Siglo XXI</label>
+                </div>
+                <span class="badge badge-primary badge-pill">4</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck5">
+                    <label class="custom-control-label" for="customCheck5">Anagrama</label>
+                </div>
+                <span class="badge badge-primary badge-pill">9</span>
+            </li>
+        </ul>
+
+
+        <ul class="list-group pt-5">
+            <h6>Generos</h6>
+
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck6">
+                    <label class="custom-control-label" for="customCheck6">Terror</label>
+                </div>
+                <span class="badge badge-primary badge-pill">3</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck7">
+                    <label class="custom-control-label" for="customCheck7">Drama</label>
+                </div>
+                <span class="badge badge-primary badge-pill">7</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck8">
+                    <label class="custom-control-label" for="customCheck8">Suspenso</label>
+                </div>
+                <span class="badge badge-primary badge-pill">8</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck9">
+                    <label class="custom-control-label" for="customCheck9">Policial</label>
+                </div>
+                <span class="badge badge-primary badge-pill">14</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck10">
+                    <label class="custom-control-label" for="customCheck10">Lectura ligera</label>
+                </div>
+                <span class="badge badge-primary badge-pill">11</span>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-center">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input " id="customCheck11">
+                    <label class="custom-control-label" for="customCheck11">Fatnasia</label>
+                </div>
+                <span class="badge badge-primary badge-pill">4</span>
+            </li>
+
+
+        </ul>
+        <div class="container-fluid h-100">
+            <div class="row w-100 align-items-center">
+                <div class="col text-center">
+                    <input type="submit" value="Buscar" class="btn btn-primary mt-2 pl-5 pr-5">
+                </div>
             </div>
-            <span class="badge badge-primary badge-pill">3</span>
-        </li>
+        </div>
 
-
-       
-
-
-
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">De Bolsillo</a>
-            <span class="badge badge-primary badge-pill">8</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Salamandra</a>
-            <span class="badge badge-primary badge-pill">14</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Siglo XXI</a>
-            <span class="badge badge-primary badge-pill">11</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Anagrama</a>
-            <span class="badge badge-primary badge-pill">4</span>
-        </li>
-    </ul>
-    <ul class="list-group pt-5">
-        <h6>Generos</h6>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Terror</a>
-            <span class="badge badge-primary badge-pill">3</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Drama</a>
-            <span class="badge badge-primary badge-pill">7</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Suspenso</a>
-            <span class="badge badge-primary badge-pill">8</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Policial</a>
-            <span class="badge badge-primary badge-pill">14</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Lectura ligera</a>
-            <span class="badge badge-primary badge-pill">11</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="#" class="font-weight-bold">Fatnasia</a>
-            <span class="badge badge-primary badge-pill">4</span>
-        </li>
-    </ul>
-
+    </form>
 
 </div>
