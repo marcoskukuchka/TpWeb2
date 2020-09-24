20f2ea6f618ab2ec47a97b5249e921d390438240
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-09-24 17:59:09 -0300

    agregar cosas

--- gracias.php
+++ gracias.php
@@ -3,24 +3,37 @@ require_once("includes/head.php");
 ?>
 <!-- fin nav -->
 
+<main>
 
-<div class="mx-auto mt-5" style="width: 700px;">
-      <div class="alert alert-success" role="alert">
-            <div class="text-center">
-                  <h4 class="alert-heading">¡Muchas Gracias!</h4>
-                  <p>LLenaste correctamente el formulario y llego a nuestra casilla. Este fue el primer paso para resolver tus dudas...</p>
-                  <hr>
-                  <p class="mb-0">En breve nos pondremos en contacto con vos..</p>
+      <section>
+            <!-- agregar imagen pluma y el bardo -->
+            <!-- agregar un parrafo  -->
 
-                  <button type="button" class="btn btn-outline-dark">
-                        <a href="index.php" class="text-success">Volver a inicio</a> </button>
+      </section>
 
-                  <button type="button" class="btn btn-outline-dark">
-                        <a href="productos.php" class="text-success"> Seguir comprando</a>
-                  </button>
+
+      <section>
+            <div class="mx-auto mt-5" style="width: 700px;">
+                  <div class="alert alert-success" role="alert">
+                        <div class="text-center">
+                              <h4 class="alert-heading">¡Muchas Gracias!</h4>
+                              <p>LLenaste correctamente el formulario y llego a nuestra casilla. Este fue el primer paso para resolver tus dudas...</p>
+                              <hr>
+                              <p class="mb-0">En breve nos pondremos en contacto con vos..</p>
+
+                              <button type="button" class="btn btn-outline-dark">
+                                    <a href="index.php" class="text-success">Volver a inicio</a> </button>
+
+                              <button type="button" class="btn btn-outline-dark">
+                                    <a href="productos.php" class="text-success"> Seguir comprando</a>
+                              </button>
+                        </div>
+                  </div>
             </div>
-      </div>
-</div>
+      </section>
+
+</main>
+
 
 <!-- footer  -->
 <?php
