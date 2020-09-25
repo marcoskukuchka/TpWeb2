55ad9d520d55405ee4d6c3e8a23cbb25a3b1f639
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-09-25 19:53:56 -0300

    mejora contacto.php

--- contacto.php
+++ contacto.php
@@ -9,17 +9,17 @@ require_once("includes/head.php");
   <form action="guardaDatos.php" method="POST">
     <div class="form-row">
       <div class="form-group col-md-6">
-      <label for="Nombre">Nombre<span>*</span></label>
+      <label for="Nombre">Nombre:</label>
             <input type="Nombre" class="form-control" id="Nombre" placeholder="Nombre" required>
       </div>
       <div class="form-group col-md-6">
-        <label for="inputapellido">Apellido<span>*</span></label>
+        <label for="inputapellido">Apellido:</label>
         <input type="apellido" class="form-control" id="apellido" placeholder="Apellido" required>
       </div>
     </div>
     <div class="form-row">
     <div class="form-group col-md-6 ">
-      <label for="inputAddress">E-mail<span>*</span></label>
+      <label for="inputAddress">E-mail:</label>
       <input type="email" class="form-control" id="email" placeholder="Direccion de email">
     </div>
 
@@ -30,27 +30,25 @@ require_once("includes/head.php");
     </div>
 
     <div class="form-row">
-        <div class="col-md-12 mb-4">
-            <label for="Asunto">Asunto:<span>*</span></label>
+        <div class="col-md-6 mb-4">
+            <label for="Asunto">Asunto:<span></span></label>
             <select class="custom-select" id="Area" name="Area" required>
-                <option selected disabled value="">Selecciona...</option>
-                <option value="Productos destacados">Productos destacados</option>
-                <option value="Alfaguara">Alfaguara</option>
-                <option value="Anagrama">Anagrama</option>
-                <option value="Planeta">Planeta</option>
-                <option value="Salamandra">Salamandra</option>
-                <option value="Comentarios Generales">Comentarios Generales</option>
+                <option selected disabled value="">Para:</option>
+                <option value="Productos destacados">ventas@elbardo.com.ar</option>
+                <option value="Alfaguara">info@elbardo.com.ar</option>
+                <option value="Anagrama">pedidos@elbardo.com.ar</option>
+                <option value="Planeta">reclamos@elbardo.com.ar</option>
             </select>
         </div>
     </div>
     <div class="form-group">
-        <label for="Mensaje">Mensaje:<span>*</span></label>
+        <label for="Mensaje">Mensaje:</label>
         <textarea class="form-control" id="Mensaje" name="Mensaje" rows="6" min="50" required></textarea>
     </div>
 
     <div class="form-group mb-8">
-    <button type="submit" class="btn btn-primary"><a href='gracias.php' class="text-success"> Enviar</a></button>
-    <button type="reset" class="btn btn-success"><a href='contacto.php' class="text-success">Limpiar</a></button>
+    <button type="reset" class="btn btn-secondary"><a href='contacto.php' class="text-dark font-weight-bold">Limpiar</a></button>
+    <button type="submit" class="btn btn-primary"><a href='gracias.php' class="text-dark font-weight-bold"> Enviar</a></button>
     </div>
   </form>
 
