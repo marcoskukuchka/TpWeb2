185177c0457fe7199b59e3374ef21af3ee56326c
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-09-26 19:49:41 -0300

    modificacion categorias

--- includes/aside.php
+++ includes/aside.php
@@ -54,35 +54,35 @@
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input " id="customCheck6">
-                        <label class="custom-control-label" for="customCheck6">Terror</label>
+                        <label class="custom-control-label" for="customCheck6">Opereta Criolla</label>
                     </div>
-                    <span class="badge badge-success badge-pill">3</span>
+                    <span class="badge badge-success badge-pill">1</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input " id="customCheck7">
-                        <label class="custom-control-label" for="customCheck7">Drama</label>
+                        <label class="custom-control-label" for="customCheck7">Ficción</label>
                     </div>
                     <span class="badge badge-success badge-pill">5</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input " id="customCheck8">
-                        <label class="custom-control-label" for="customCheck8">Suspenso</label>
+                        <label class="custom-control-label" for="customCheck8">Terror</label>
                     </div>
                     <span class="badge badge-success badge-pill">4</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input " id="customCheck9">
-                        <label class="custom-control-label" for="customCheck9">Policial</label>
+                        <label class="custom-control-label" for="customCheck9">Novela Historica</label>
                     </div>
                     <span class="badge badge-success badge-pill">6</span>
                 </li>
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <div class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input " id="customCheck10">
-                        <label class="custom-control-label" for="customCheck10">Lectura ligera</label>
+                        <label class="custom-control-label" for="customCheck10">Biografía</label>
                     </div>
                     <span class="badge badge-success badge-pill">3</span>
                 </li>
@@ -93,7 +93,27 @@
                     </div>
                     <span class="badge badge-success badge-pill">4</span>
                 </li>
-
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input " id="customCheck11">
+                        <label class="custom-control-label" for="customCheck11">Ensayos</label>
+                    </div>
+                    <span class="badge badge-success badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input " id="customCheck11">
+                        <label class="custom-control-label" for="customCheck11">Cuentos</label>
+                    </div>
+                    <span class="badge badge-success badge-pill">1</span>
+                </li>
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input " id="customCheck11">
+                        <label class="custom-control-label" for="customCheck11">Policial</label>
+                    </div>
+                    <span class="badge badge-success badge-pill">3</span>
+                </li>
             </ul>
             <div class="container-fluid h-100">
                 <div class="row w-100 align-items-center">
