4583ca3aadaf4b3a11bd4ab38f5dd8ee08d5e03c
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-10-15 19:36:20 -0300

    Buscador

--- includes/aside.php
+++ includes/aside.php
@@ -5,116 +5,88 @@
         <h2 class="pt-3 text-center text-uppercase">El Bardo Inmortal</h1>
 
     </div>
+<?php
+
+$ESTE_ARCHIVO  = 'ejemplo.php';
+
+// Analizo los parámetros de invocación a esta página
+    if (isset($_REQUEST['id_editorial']))
+        $id_editorial = $_REQUEST['id_editorial'];
+    else
+        $id_editorial = array();
+
+    if (isset($_REQUEST['id_genero']))
+        $id_genero = $_REQUEST['id_genero'];
+    else
+        $id_genero = array();
+
+//guardo json en array
+$a_multi_genero = json_decode(file_get_contents('./json/genero.json'), true);
+$a_multi_editorial = json_decode(file_get_contents('./json/editorial.json'), true);
+$a_multi_productos = json_decode(file_get_contents('./json/descripcionproducto.json'), true);
+
+?>
+
+
 
     <div>
-        <form>
+        
+         <form action="" method="get">
+<!-- buscador editoriales -->
+          <ul class="list-group pt-5">
+               <h3>Editoriales</h3>
+               <?php
+               $i = 1;
+               foreach ($a_multi_editorial as $a_editorial) {
+                if (in_array($a_editorial['id_editorial'],$id_editorial))
+                    $checked = 'checked="chequed"';
+                else
+                    $checked = '';
+                $i = $i + 1;
+                ?>
+               <li class="list-group-item d-flex justify-content-between align-items-center">
+               <div class="custom-control custom-checkbox">
+               <?php 
+               echo '<input type="checkbox" class="custom-control-input" id="' . $i .'" name="id_editorial[]" value = "' . $a_editorial['id_editorial'] . '" ' . $checked . '>';
+               
+                       echo '<label class="custom-control-label" for="'. $i .'">'. $a_editorial['nombre'].'</label>';
+                        ?> 
+                    </div>
+                    <span class="badge badge-success badge-pill">4</span>
+                </li>
+            </ul>
+                <?php
+                }
+                ?> 
 
+<!-- Buscador Generos -->
             <ul class="list-group pt-5">
-                <h3>Editoriales</h3>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck1">
-                        <label class="custom-control-label" for="customCheck1">Planeta</label>
+               <h3>Generos</h3>
+               <?php
+               $i = 1;
+               foreach ($a_multi_genero as $a_genero) {
+                if (in_array($a_genero['id_genero'],$id_genero))
+                    $checked = 'checked="chequed"';
+                else
+                    $checked = '';
+                $i = $i + 10;
+                ?>
+               <li class="list-group-item d-flex justify-content-between align-items-center">
+               <div class="custom-control custom-checkbox">
+               <?php 
+               echo '<input type="checkbox" class="custom-control-input" id="' . $i .'" name="id_genero[]" value = "' . $a_genero['id_genero'] . '" ' . $checked . '>';
+               
+                       echo '<label class="custom-control-label" for="'. $i .'">'. $a_genero['nombre'].'</label>';
+                        ?> 
                     </div>
                     <span class="badge badge-success badge-pill">4</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck2">
-                        <label class="custom-control-label" for="customCheck2">De Bolsillo</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck3">
-                        <label class="custom-control-label" for="customCheck3">Salamandra</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck4">
-                        <label class="custom-control-label" for="customCheck4">Alfaguara</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck5">
-                        <label class="custom-control-label" for="customCheck5">Anagrama</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">5</span>
                 </li>
             </ul>
+                <?php
+                }
+                ?> 
 
-            <ul class="list-group pt-3">
-                <h3>Generos</h3>
 
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck6">
-                        <label class="custom-control-label" for="customCheck6">Opereta Criolla</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">1</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck7">
-                        <label class="custom-control-label" for="customCheck7">Ficción</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">5</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck8">
-                        <label class="custom-control-label" for="customCheck8">Terror</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">4</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck9">
-                        <label class="custom-control-label" for="customCheck9">Novela Historica</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">6</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck10">
-                        <label class="custom-control-label" for="customCheck10">Biografía</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">3</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck11">
-                        <label class="custom-control-label" for="customCheck11">Fantasia</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">4</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck12">
-                        <label class="custom-control-label" for="customCheck12">Ensayos</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">1</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck13">
-                        <label class="custom-control-label" for="customCheck13">Cuentos</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">1</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input " id="customCheck14">
-                        <label class="custom-control-label" for="customCheck14">Policial</label>
-                    </div>
-                    <span class="badge badge-success badge-pill">3</span>
-                </li>
-            </ul>
             <div class="container-fluid h-100">
                 <div class="row w-100 align-items-center">
                     <div class="col text-center">
