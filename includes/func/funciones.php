c7bb9b36c31d6f9ed491368cf4b28a6861c6bf20
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-10-18 15:04:13 -0300

    contador de estrellas

--- includes/func/funciones.php
+++ includes/func/funciones.php
@@ -10,10 +10,24 @@ function contarItems($array, $indice, $valorBuscado){
     return $contador;
 }
 
-/* function calculoEstrellas ($array, $valoracion, $producto)
-    foreach ($array as $items_array) {
-        if ($items_array[$valoracion] == ) {
-            # code...
+
+function muestraEstrellas($array, $indice, $producto_buscado){
+
+    $suma = 0;
+    $contador = 0;
+    foreach ($array as $item_array) {
+        if ($item_array[$indice] == $producto_buscado){
+            $suma = $suma + $item_array['valoracion'];
+            $contador++;
         }
-    } */
+    }
+    
+    $promedio = $suma / $contador;
+    $estrellas = "";
+    for ($i=0; $i < $promedio ; $i++) { 
+        $estrellas = $estrellas . '&#9733; ';
+        
+    }
+    return $estrellas;
+}
 ?>
