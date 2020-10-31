7b4c12e8bf243e83d18cd886d9d9611e648747f2
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-10-31 00:53:55 -0300

    variables aux mas correcciones para implementar carpetas por productos

--- includes/listaProductos.php
+++ includes/listaProductos.php
@@ -9,20 +9,22 @@ require_once("includes/func/funciones.php");
        ( ( in_array($a_producto['id_genero'],$id_genero) || empty($id_genero) )))
        
     {
-        $url = "<a href='detallesProducto.php?id={$a_producto['id_producto']}'>";
-        $textoAlt = $a_producto['nombre'];
+
+        $id_prod = $a_producto['id_producto'];
         $precio = $a_producto['precio'];
         $nombre = $a_producto['nombre'];
         $descripcion = cortar_palabras($a_producto['descripcion_larga'], 70);
         $totalEstrellas = muestraEstrellas($a_multi_comentario,'id_producto', $a_producto['id_producto']);
+        $img = 'img\\productos\\'.$id_prod.'\\'.$id_prod.'_mini.jpg';
+      
 ?>
 
     <div class="col-lg-4 col-md-6 mb-4">
     <div class="card h-100">
-            <?php echo $url ?><img class="card-img-top"  src="<?php echo $a_producto['url'];?>" alt="<?php echo $textoAlt?>"></a>
+            <a href="detallesProducto.php?id=<?php echo $id_prod; ?>"><img class="card-img-top"  src="<?php echo $img;?>" alt="<?php echo $nombre;?>"></a>
             <div class="card-body">
                 <h4 class="card-title">
-                <?php echo $url?><?php echo $nombre?></a>
+                <a href="detallesProducto.php?id=<?php echo $id_prod;?>"><?php echo $nombre?></a>
                 </h4>
                 <h5><?php echo $precio;?></h5>
                 <p class="card-text"><?php echo $descripcion?></p>
