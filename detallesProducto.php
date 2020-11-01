dbfedad79bb287f0ba677a849dc120a71d3cbd3f
marcoskukuchka <marcos.kukuchka@gmail.com>
2020-11-01 20:30:28 -0300

    modal

--- detallesProducto.php
+++ detallesProducto.php
@@ -85,10 +85,35 @@ require_once("includes/head.php");
                  }
                }
             ?>
-            
-            <?php
-            require_once("includes/modal.php")
-            ?>
+            <h5>Dejanos tu reseña</h5>
+            <form>
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">e-mail</label>
+                        <input type="email" class="form-control" id="email" placeholder="Direccion de email">
+                    </div>
+                    <div class="form-group">
+                        <label for="message-text" class="col-form-label">Reseña:</label>
+                        <textarea class="form-control" id="message-text"></textarea>
+                    </div>
+                   
+                        <label for="Asunto">Calificación</label>
+                        <select class="custom-select mb-3" id="Area" name="Area" required>
+                            <option selected disabled value="5 estrellas"><span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9733;</span></option>
+                            <option value="4 estrellas"><span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span></option>
+                            <option value="3 estrellas"><span class="text-warning">&#9733; &#9733; &#9733; &#9734; &#9734;</span></option>
+                            <option value="2 estrellas"><span class="text-warning">&#9733; &#9733; &#9734; &#9734; &#9734;</span></option>
+                            <option value="1 estrella"><span class="text-warning">&#9733; &#9734; &#9734; &#9734; &#9734;</span></option>
+                        </select>
+                    
+                    <div class="mt-3">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">
+                            cerrar
+                        </button>
+                        <button class="btn btn-warning" type="button">
+                            aceptar
+                        </button>
+                    </div>
+                </form>
           </div>
         </div>
       </div>
